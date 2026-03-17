4de67c5303807f1b6331156b2811f85b6d7fa7a4
Arthur Dzieniszewski <arthurdzieniszewski@gmail.com>
2026-03-17 09:56:50 -0400

    Fix Android compile import for testing PR

--- app/src/main/java/io/proxyt/hotspot/MainActivity.kt
+++ app/src/main/java/io/proxyt/hotspot/MainActivity.kt
@@ -23,6 +23,7 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
+import androidx.core.content.ContextCompat
 import androidx.core.widget.doAfterTextChanged
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.checkbox.MaterialCheckBox
